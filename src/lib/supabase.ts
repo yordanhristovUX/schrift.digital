216cfa465b8708caee341e81cecb4e1871444758
yordanhristovUX <61191322+yordanhristovUX@users.noreply.github.com>
2025-05-28 18:45:20 +0300

    Create environment variables file

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dmVjYWZicWVzcmd6aGNxYWxxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODAxOTg1OSwiZXhwIjoyMDYzNTk1ODU5fQ.ofaBbe_VCiFaLaA9EIdZgYZeKtldhsHNsd7Kvyv62KY';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl) {
-  throw new Error('Missing Supabase URL environment variable');
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables');
 }
 
-export const supabase = createClient(supabaseUrl, supabaseServiceKey, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: true,
     persistSession: true,
