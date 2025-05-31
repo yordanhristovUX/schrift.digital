d6f33874c549d4353c5b2b6e38365d5b8a7ab968
yordanhristovUX <61191322+yordanhristovUX@users.noreply.github.com>
2025-05-31 17:47:52 +0300

    Set up Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
@@ -34,4 +35,6 @@ export default {
     }
   },
   plugins: []
-};
+}
+
+export default config;
